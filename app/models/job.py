a4f1db32d9a32fc1b95fd8b605f55c05dd3c1d75
GuiDev-01 <guilhermesantosdev01@gmail.com>
2025-09-14 00:06:31 -0300

    fix: improve code formatting in Job model

--- app/models/job.py
+++ app/models/job.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class Job(Base):
     __tablename__ = "jobs"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True) # Integer column, primary key
-    title: Mapped[str] = mapped_column(String) # String column for job title
-    company:Mapped[str] = mapped_column(String) # String column for company name
-    work_modality: Mapped[str]= mapped_column(String) #String Column for work modality (remote, hybrid, on-site)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)  # Integer column, primary key
+    title: Mapped[str] = mapped_column(String)  # String column for job title
+    company: Mapped[str] = mapped_column(String)  # String column for company name
+    work_modality: Mapped[str] = mapped_column(String)  # String Column for work modality (remote, hybrid, on-site)
